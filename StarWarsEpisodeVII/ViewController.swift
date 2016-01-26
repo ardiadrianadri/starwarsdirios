aa5e97ad1ff240b7aa76ddb61ea9898c45c1bfc8
Adrian Ferreres Esteller <ardi@adrians-mac.local>
2016-01-26 22:37:06 +0100

    Initial Commit

---
+++ StarWarsEpisodeVII/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarWarsEpisodeVII
+//
+//  Created by Adrian Ferreres Esteller on 26/01/16.
+//  Copyright © 2016 Adrian Ferreres Esteller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
